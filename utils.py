2597bb5cc3923ff0f9f9b908aea9a84f7b845db8
alyxx <75878508+Alyxx-The-Sniper@users.noreply.github.com>
2024-01-16 17:43:08 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
+utils.py
 """
 Contains various utility functions for PyTorch model training and saving.
 """
+
+#### Table of content #####
+# 1. save_model
+# 2. Pred and Plot Image
+# 3. plot_model_performance (2 model)
+# 4. Display random dataset images
+# 5. set seeds
+# 6. Plot loss curves of a model
+# 7.  Download data version 1
+# 8.  Download data version 2
+# 9. pred_and_store
+
+
+
+
+####################################################
+# 1. save_model
 import torch
 from pathlib import Path
 
@@ -33,3 +51,385 @@ def save_model(model: torch.nn.Module,
   print(f"[INFO] Saving model to: {model_save_path}")
   torch.save(obj=model.state_dict(),
              f=model_save_path)
+
+
+####################################################
+ # 2. Pred and Plot Image
+"""
+Utility functions to make predictions.
+
+Main reference for code creation: https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set 
+"""
+import torch
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from typing import List, Tuple
+
+from PIL import Image
+
+# Set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Predict on a target image with a target model
+# Function created in: https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set
+def pred_and_plot_image(
+    model: torch.nn.Module,
+    class_names: List[str],
+    image_path: str,
+    image_size: Tuple[int, int] = (224, 224),
+    transform: torchvision.transforms = None,
+    device: torch.device = device,
+):
+    """Predicts on a target image with a target model.
+
+    Args:
+        model (torch.nn.Module): A trained (or untrained) PyTorch model to predict on an image.
+        class_names (List[str]): A list of target classes to map predictions to.
+        image_path (str): Filepath to target image to predict on.
+        image_size (Tuple[int, int], optional): Size to transform target image to. Defaults to (224, 224).
+        transform (torchvision.transforms, optional): Transform to perform on image. Defaults to None which uses ImageNet normalization.
+        device (torch.device, optional): Target device to perform prediction on. Defaults to device.
+    """
+
+    # Open image
+    img = Image.open(image_path)
+
+    # Create transformation for image (if one doesn't exist)
+    if transform is not None:
+        image_transform = transform
+    else:
+        image_transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+    ### Predict on image ###
+
+    # Make sure the model is on the target device
+    model.to(device)
+
+    # Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+        transformed_image = image_transform(img).unsqueeze(dim=0)
+
+        # Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(transformed_image.to(device))
+
+    # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    # Plot image with predicted label and probability
+    plt.figure()
+    plt.imshow(img)
+    plt.title(
+        f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}"
+    )
+    plt.axis(False)
+
+####################################################
+# 3. plot_model_performance (2 model)
+import matplotlib.pyplot as plt
+def plot_model_performance(model_0_df, model_1_df):
+    '''This function takes two dataframes with the same number of epochs'''
+
+    # Setup a plot
+    plt.figure(figsize=(15, 10))
+
+    # Get number of epochs
+    epochs = range(len(model_0_df))
+
+    # Plot train loss
+    plt.subplot(2, 2, 1)
+    plt.plot(epochs, model_0_df["train_loss"], label="Model 0")
+    plt.plot(epochs, model_1_df["train_loss"], label="Model 1")
+    plt.title("Train Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot test loss
+    plt.subplot(2, 2, 2)
+    plt.plot(epochs, model_0_df["test_loss"], label="Model 0")
+    plt.plot(epochs, model_1_df["test_loss"], label="Model 1")
+    plt.title("Test Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot train accuracy
+    plt.subplot(2, 2, 3)
+    plt.plot(epochs, model_0_df["train_acc"], label="Model 0")
+    plt.plot(epochs, model_1_df["train_acc"], label="Model 1")
+    plt.title("Train Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot test accuracy
+    plt.subplot(2, 2, 4)
+    plt.plot(epochs, model_0_df["test_acc"], label="Model 0")
+    plt.plot(epochs, model_1_df["test_acc"], label="Model 1")
+    plt.title("Test Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Display the plot
+    plt.show()
+
+###############################################################################
+# 4. Display random dataset images
+def display_random_dataset_images(dataset: torch.utils.data.dataset.Dataset,
+                                  classes: List[str] = None,
+                                  n: int = 10,
+                                  display_shape: bool = True,
+                                  seed: int = None):
+    # 2. Adjust display if n too high
+    if n > 10:
+        n = 10
+        display_shape = False
+        print(f"For display purposes, n shouldn't be larger than 10, setting to 10 and removing shape display.")
+
+    # 3. Set random seed
+    if seed:
+        random.seed(seed)
+
+    # 4. Get random sample indexes
+    random_samples_idx = random.sample(range(len(dataset)), k=n)
+
+    # 5. Setup plot
+    plt.figure(figsize=(16, 8))
+
+    # 6. Loop through samples and display random samples
+    for i, targ_sample in enumerate(random_samples_idx):
+        targ_image, targ_label = dataset[targ_sample][0], dataset[targ_sample][1]
+
+        # 7. Adjust image tensor shape for plotting: [color_channels, height, width] -> [color_channels, height, width]
+        targ_image_adjust = targ_image.permute(1, 2, 0)
+
+        # Plot adjusted samples
+        plt.subplot(1, n, i + 1)
+        plt.imshow(targ_image_adjust)
+        plt.axis("off")
+        if classes:
+            title = f"class: {classes[targ_label]}"
+            if display_shape:
+                title = title + f"\nshape: {targ_image_adjust.shape}"
+        plt.title(title)
+
+###########################################################
+# 5. set seeds
+def set_seeds(seed: int = 42):
+    """Sets random sets for torch operations.
+
+    Args:
+        seed (int, optional): Random seed to set. Defaults to 42.
+    """
+    # Set the seed for general torch operations
+    torch.manual_seed(seed)
+    # Set the seed for CUDA torch operations (ones that happen on the GPU)
+    torch.cuda.manual_seed(seed)
+
+
+
+#################################################################
+# 6. Plot loss curves of a model
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+    """
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
+
+    accuracy = results["train_acc"]
+    test_accuracy = results["test_acc"]
+
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+
+#################################################################
+# 7. Download data version 1
+def download_data_V1(source: str,
+                     destination: str,
+                     remove_source: bool = True) -> Path:
+    """Downloads a zipped dataset from source and unzips to destination.
+
+    Args:
+        source (str): A link to a zipped file containing data.
+        destination (str): A target directory to unzip data to.
+        remove_source (bool): Whether to remove the source after downloading and extracting.
+
+    Returns:
+        pathlib.Path to downloaded data.
+
+    Example usage:
+        download_data(source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
+                      destination="pizza_steak_sushi")
+    """
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination
+
+    # If the image folder doesn't exist, download it and prepare it...
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+
+        # Download pizza, steak, sushi data
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as f:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            f.write(request.content)
+
+        # Unzip pizza, steak, sushi data
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...")
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file
+        if remove_source:
+            os.remove(data_path / target_file)
+
+    return image_path
+
+
+#################################################################################################
+# 8.  Download data version 2
+def download_data_V2(txtfile,
+                     source,
+                     destination,
+                     percentage: float,
+                     class_list=[],
+                     random_seed=42):
+    """ Downloads a sample list from a text file.
+
+    Args:
+        txtfile: A link to a txtfile containing data sample name list.
+        source: The source destination of the images
+        percentage: percatage to doanload
+        class_list: List of food class to be downloaded.
+        random_seed: default is 42
+    """
+
+    random.seed(random_seed)
+    class_types = defaultdict(list)
+    with open(txtfile, "r") as file:
+        lines = [line.strip() for line in file.readlines()]
+        for l in lines:
+            class_type = l.split("/")
+            if class_type[0] in class_list:
+                class_types[class_type[0]].append(class_type[1] + ".jpg")
+
+    for i in class_types.keys():
+        print("  " + i, end="  ")
+        if not os.path.exists(os.path.join(destination, i)):
+            os.makedirs(os.path.join(destination, i))
+
+        # Retrieve random sample of % of the data
+        data_sample = random.sample(class_types[i], k=int(len(class_types[i]) * percentage))
+
+        for n in data_sample:
+            copy(os.path.join(source, i, n), os.path.join(destination, i, n))
+
+#############################################################################
+# 9. pred_and_store
+#  Create a function to return a list of dictionaries with sample,
+#  truth label, prediction, prediction probability and prediction time
+
+import pathlib
+import torch
+
+from PIL import Image
+from timeit import default_timer as timer
+from tqdm.auto import tqdm
+from typing import List, Dict
+
+def pred_and_store(paths: List[pathlib.Path],
+                   model: torch.nn.Module,
+                   transform: torchvision.transforms,
+                   class_names: List[str],
+                   device: str = "cuda" if torch.cuda.is_available() else "cpu") -> List[Dict]:
+    # 2. Create an empty list to store prediction dictionaires
+    pred_list = []
+
+    # 3. Loop through target paths
+    for path in tqdm(paths):
+        # 4. Create empty dictionary to store prediction information for each sample
+        pred_dict = {}
+
+        # 5. Get the sample path and ground truth class name
+        pred_dict["image_path"] = path
+        class_name = path.parent.stem
+        pred_dict["class_name"] = class_name
+
+        # 6. Start the prediction timer
+        start_time = timer()
+
+        # 7. Open image path
+        img = Image.open(path)
+
+        # 8. Transform the image, add batch dimension and put image on target device
+        transformed_image = transform(img).unsqueeze(0).to(device)
+
+        # 9. Prepare model for inference by sending it to target device and turning on eval() mode
+        model.to(device)
+        model.eval()
+
+        # 10. Get prediction probability, predicition label and prediction class
+        with torch.inference_mode():
+            pred_logit = model(transformed_image)  # perform inference on target sample
+            pred_prob = torch.softmax(pred_logit, dim=1)  # turn logits into prediction probabilities
+            pred_label = torch.argmax(pred_prob, dim=1)  # turn prediction probabilities into prediction label
+            pred_class = class_names[pred_label.cpu()]  # hardcode prediction class to be on CPU
+
+            # 11. Make sure things in the dictionary are on CPU (required for inspecting predictions later on)
+            pred_dict["pred_prob"] = round(pred_prob.unsqueeze(0).max().cpu().item(), 4)
+            pred_dict["pred_class"] = pred_class
+
+            # 12. End the timer and calculate time per pred
+            end_time = timer()
+            pred_dict["time_for_pred"] = round(end_time - start_time, 4)
+
+        # 13. Does the pred match the true label?
+        pred_dict["correct"] = class_name == pred_class
+
+        # 14. Add the dictionary to the list of preds
+        pred_list.append(pred_dict)
+
+    # 15. Return list of prediction dictionaries
+    return pred_list
